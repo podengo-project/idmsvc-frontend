fcec17a9759c4db840d6268157e9de94954a83c9
Petr Vobornik <pvoborni@redhat.com>
2024-04-29 14:41:21 +0000

    refactor: useNotification hook
    Add an utility hook which provides easy-to use methods for displaying
    notifications via `<NotificationsPortal />`.
    
    Prep for: HMS-2997, HMS-2998
    
    Signed-off-by: Petr Vobornik <pvoborni@redhat.com>

---
+++ src/Hooks/useNotification.tsx
@@ -0,0 +1,25 @@
+import { AlertVariant } from '@patternfly/react-core';
+import { addNotification } from '@redhat-cloud-services/frontend-components-notifications/redux';
+import React from 'react';
+import { useDispatch } from 'react-redux';
+
+export interface NotificationPayload {
+  title: React.ReactNode | string;
+  variant?: AlertVariant;
+  description?: React.ReactNode | string;
+  id?: string | number;
+  dismissable?: boolean;
+}
+
+export default function useNotification() {
+  const dispatch = useDispatch();
+  const notify = (payload: NotificationPayload & { variant: AlertVariant }) => dispatch(addNotification(payload));
+
+  const notifyError = (payload: NotificationPayload) => notify({ variant: AlertVariant.danger, ...payload });
+
+  const notifySuccess = (payload: NotificationPayload) => notify({ variant: AlertVariant.success, ...payload });
+
+  const notifyWarning = (payload: NotificationPayload) => notify({ variant: AlertVariant.warning, ...payload });
+
+  return { notify, notifyError, notifySuccess, notifyWarning };
+}
